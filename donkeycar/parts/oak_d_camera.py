8f9932aa991e91e78206577ebfab61d953c0e5c2
btrinite <btrinite@free.fr>
2023-04-19 14:45:07 +0200

    fix

--- donkeycar/parts/oak_d_camera.py
+++ donkeycar/parts/oak_d_camera.py
@@ -390,7 +390,7 @@ class OakDCamera:
         if self.queue_xout is not None:
             data_xout = self.queue_xout.get() # blocking
             image_data_xout = data_xout.getFrame()
-            self.cam_ts = time_ns()
+            self.cam_ts = time.time_ns()
             self.frame_xout = np.moveaxis(image_data_xout,0,-1)
 
             if logger.isEnabledFor(logging.DEBUG):
