d1c516faf39e6da500f625b0a5f6004169d219e1
Benoit TRINITE <benoit.trinite@renault.com>
2024-03-22 16:31:53 +0100

    no accel when recording manual driving

--- donkeycar/parts/robocars_hat_ctrl.py
+++ donkeycar/parts/robocars_hat_ctrl.py
@@ -411,7 +411,7 @@ class RobocarsHatInCtrl(metaclass=Singleton):
     def run_threaded(self):
         user_throttle, user_steering, user_mode, recording = self.processAltModes ()
         if self.cfg.RECORD_DEFAULT_BEHAVIOR:
-            return user_steering, user_throttle, user_mode, recording, user_steering, 'center', 1.0
+            return user_steering, user_throttle, user_mode, recording, user_steering, 'center', 0.0
         else:
             return user_steering, user_throttle, user_mode, recording
 
@@ -419,7 +419,7 @@ class RobocarsHatInCtrl(metaclass=Singleton):
         self.getCommand()
         user_throttle, user_steering, user_mode, recording = self.processAltModes ()
         if self.cfg.RECORD_DEFAULT_BEHAVIOR:
-            return user_steering, user_throttle, user_mode, recording, user_steering, 'center', 1.0
+            return user_steering, user_throttle, user_mode, recording, user_steering, 'center', 0.0
         else:
             return user_steering, user_throttle, user_mode, recording
     
