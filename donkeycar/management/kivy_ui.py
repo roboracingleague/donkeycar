Wrong index when clearing mask and deleted index exists

donkeycar/management/kivy_ui.py
@@ -1029,7 +1029,7 @@ class AnnotateScreen(Screen):
             self.current_record = tub_screen().ids.tub_loader.records[0]
             if self.mask_records:
                 self.current_mask_record=self.mask_records[0]
-            self.update_image_with_mask(self.current_record, index=self.index)
+            self.update_image_with_mask(self.current_record, index=self.current_record.underlying['_index'])
 
     def get_empty_mask(self):
         pil_image_ref = PilImage.fromarray(tub_screen().ids.tub_loader.records[0].image())
@@ -1183,14 +1183,14 @@ class AnnotateScreen(Screen):
     def reset_mask(self, left=False):
         default_mask = self.get_empty_mask()
         if self.mask_records:
-            mask_record = self.mask_records[self.index]
+            mask_record = self.mask_records[self.current_record.underlying['_index']]
             if left:
                 mask_record={'left_mask':default_mask, 'right_mask':mask_record.underlying['right_mask'], 'left_poi':[], 'right_poi':mask_record.underlying['right_poi']}
             else:
                 mask_record={'left_mask':mask_record.underlying['left_mask'], 'right_mask':default_mask, 'left_poi':mask_record.underlying['left_poi'], 'right_poi':[]}
-            self.mask_tub.write_record(mask_record, index=self.index)
+            self.mask_tub.write_record(mask_record, index=self.current_record.underlying['_index'])
             self.status(f'Mask tub record index {self.index} reinitialized')
-            self.update_image_with_mask(self.current_record, index=self.index)
+            self.update_image_with_mask(self.current_record, index=self.current_record.underlying['_index'])
 
     def set_auto_mask(self):
         if (self.auto_mask):
@@ -1281,9 +1281,9 @@ class AnnotateScreen(Screen):
         for index, rec in enumerate (self.mask_records[starting_index:]): 
             mask_left = rec.image(key='left_mask', as_nparray=True, format='NPY', reload=True, image_base_path='left')
             mask_right = rec.image(key='right_mask', as_nparray=True, format='NPY', reload=True, image_base_path='right')
-            if (not np.any(mask_left)) and (not np.any(mask_right)):
+            if (not np.any(mask_left)) or (not np.any(mask_right)):
                 new_index = index+starting_index
-                self.status(f"No mask set on this image")
+                self.status(f"No both mask set on this image")
                 break
             nb_left_area = has_multiple_areas (mask_left)
             nb_right_area = has_multiple_areas (mask_right)