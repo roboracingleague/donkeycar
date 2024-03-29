89eeab3a50e8cc7a67dd216b79fcd50056c10d6c
Muldec <34677746+Muldec@users.noreply.github.com>
2024-03-29 16:48:27 +0100

    Add limit-worskpace for detector

--- donkeycar/tools/tensorrt/build_save_trt_engine.py
+++ donkeycar/tools/tensorrt/build_save_trt_engine.py
@@ -1,20 +1,23 @@
 
 """
-Generate tensorrt model from onnx 
+Generate tensorrt model from onnx
 Usage:
-  build_save_trt_engine.py (--onnx <onnxfile>) (--savedtrt <trtfile>)
+  build_save_trt_engine.py (--onnx <onnxfile>) (--savedtrt <trtfile>) [--limit-workspace]
 
   # build_save_trt_engine.py --onnx new_model.onnx --savedtrt new_model.trt
+Options:
+  -h --help                Show this screen.
+     --limit-workspace     Specify if the workspace size must be limited
 """
 
 from docopt import docopt
 from donkeycar.tools.tensorrt import engine as eng
 from onnx import ModelProto
 
-def build(onnx_path,engine_name):
+def build(onnx_path,engine_name, limit_workspace):
     # engine_name = "~/mycar/models/new_model.plan"
     # onnx_path = "~/mycar/models/new_model.onnx"
-    batch_size = 1 
+    batch_size = 1
 
     model = ModelProto()
     with open(onnx_path, "rb") as f:
@@ -24,10 +27,13 @@ def build(onnx_path,engine_name):
     d1 = model.graph.input[0].type.tensor_type.shape.dim[2].dim_value
     d2 = model.graph.input[0].type.tensor_type.shape.dim[3].dim_value
     shape = [batch_size , d0, d1 ,d2]
-    engine = eng.build_engine(onnx_path, shape= shape)
+    engine = eng.build_engine(onnx_path, limit_workspace, shape= shape)
     eng.save_engine(engine, engine_name)
 
 if __name__=='__main__':
     args = docopt(__doc__)
+    onnx_model_path = args['<onnx_model_path>']
+    trt_engine_path = args['<trt_engine_path>']
+    limit_workspace = args['--limit-workspace']
 
-    build(args['<onnxfile>'],args['<trtfile>'])
+    build(onnx_model_path,trt_engine_path, limit_workspace)
--- donkeycar/tools/tensorrt/engine.py
+++ donkeycar/tools/tensorrt/engine.py
@@ -7,30 +7,31 @@ trt_runtime = trt.Runtime(TRT_LOGGER)
 #ctx = pycuda.autoinit.context
 #trt.init_libnvinfer_plugins(None, "")
 
-def build_engine(onnx_path, shape = [1,224,224,3]):
+def build_engine(onnx_path, limit_workspace, shape = [1,224,224,3]):
 
    """
    This is the function to create the TensorRT engine
    Args:
-      onnx_path : Path to onnx_file. 
-      shape : Shape of the input of the ONNX file. 
+      onnx_path : Path to onnx_file.
+      shape : Shape of the input of the ONNX file.
   """
    print(f"building trt engine with shape {shape}")
-    
+
    with trt.Builder(TRT_LOGGER) as builder, builder.create_network(1) as network, builder.create_builder_config() as config, trt.OnnxParser(network, TRT_LOGGER) as parser:
-       
+
        # config.max_workspace_size = (256 << 20) # 256MB
-       #config.max_workspace_size = 1 << 30   # 1GB
+       if limit_workspace:
+           config.max_workspace_size = 1 << 30  # 1GB
        #config.set_flag(trt.BuilderFlag.FP16)
 
        with open(onnx_path, 'rb') as model:
            parser.parse(model.read())
-       
+
        network.get_input(0).shape = shape
-       
+
        #builder.max_batch_size = 1
        engine = builder.build_engine(network, config)
-       
+
        return engine
 
 def save_engine(engine, file_name):
